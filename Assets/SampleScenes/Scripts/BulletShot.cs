1a6759432c87a21fcddf594d9860dad6f7a3329a
bambandiaye181 <bambandiaye181@gmail.com>
2018-10-30 19:34:09 +0100

    saws rotation

---
+++ Assets/SampleScenes/Scripts/BulletShot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletShot : MonoBehaviour
+{
+    EnableShoot es;
+    public GameObject bullet;
+    public GameObject spawnPoint;
+    private BoxCollider2D bc;
+
+    // Use this for initialization
+    void Start()
+    {
+        bc = GetComponent<BoxCollider2D>();
+        es = GetComponent<EnableShoot>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bullet, spawnPoint.transform.position, spawnPoint.transform.rotation);
+          
+        }
+    }
+}
---
+++ Assets/SampleScenes/Scripts/EnableShoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableShoot : MonoBehaviour {
+
+    public bool enableShoot=false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("ok");
+            enableShoot = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            enableShoot = false;
+        }
+    }
+}
